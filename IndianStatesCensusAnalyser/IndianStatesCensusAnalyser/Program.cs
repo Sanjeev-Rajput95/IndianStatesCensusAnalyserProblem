219754b7646e460d070ade1b0993f12ca84de552
sanjeev <sanjeevsingh.77d@gmail.com>
2022-12-11 15:52:43 +0530

    Welcome Message

---
+++ IndianStatesCensusAnalyser/IndianStatesCensusAnalyser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStatesCensusAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t===================== WELCOME TO INDIAN STATES CENSUS ANALYSER PROBLEM =====================\t");
+        }
+    }
+}
